a838790c8d0914105d96319f8411f05bbec356a3
Samuel Fletcher <49329336+SamuelFlet@users.noreply.github.com>
2022-01-24 12:30:00 -0500

    Stuff done

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,26 @@
-<script src="https://unpkg.com/vue@2.4.2/dist/vue.min.js"></script>
 <template>
   <div id="app">
     <input class="shadow-xl" v-model="name" />
     <button class="shadow-xl" @click="fetchAxios">Get input field value</button>
 
     <div class="box-border flex justify-center shadow-xl" id="card">
-      <p v-if="results.current" class="text-7xl">{{results.current.temp_c}}°C</p>
-      <img v-for="cons in results.current" :key="cons.condition" class="object-center" :src="cons.icon">
+      <p v-if="results.current" class="text-7xl">
+        {{ results.current.temp_c }}°C
+      </p>
+      <img
+        v-for="cons in results.current"
+        :key="cons.condition"
+        class="object-center"
+        :src="cons.icon"
+      />
     </div>
-
   </div>
 </template>
-
 
 <script>
 import axios from "axios";
 export default {
-  name: 'IndexPage',
+  name: "IndexPage",
   data() {
     return {
       results: [],
@@ -25,11 +29,17 @@ export default {
   },
   methods: {
     fetchAxios() {
-      axios.post("http://api.weatherapi.com/v1/current.json?Key=6cc51e6401484112b72142155221701&q=" + this.name)
-        .then((response) => {this.results = response.data;});
+      axios
+        .post(
+          "http://api.weatherapi.com/v1/current.json?Key=6cc51e6401484112b72142155221701&q=" +
+            this.name
+        )
+        .then((response) => {
+          this.results = response.data;
+        });
     },
   },
-}
+};
 </script>
 
 <style>
@@ -43,8 +53,8 @@ export default {
   margin-top: 60px;
 }
 
-#card{
-  margin-top:50px;
+#card {
+  margin-top: 50px;
   margin-left: 30%;
   margin-right: 30%;
 }
